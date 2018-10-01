e8c25b74da19a39ec28069f487c8fd302bc74137
Luke De Feo <luke.defeo@just-eat.com>
2018-10-01 21:03:25 +0100

    added redux dev tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,19 @@
-import { createStore, combineReducers } from 'redux'
-import reducer  from './reducers/places'
+import { createStore, combineReducers, compose } from 'redux'
+import reducer from './reducers/places'
 
 const rootReducer = combineReducers({
-    places: reducer
+  places: reducer
 });
 
+let composeEnhancers = compose
+
+if (__DEV__) { //__DEV__ is a special global exposed by react-native
+  //this enable redux dev tools
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+}
+
 const configureStore = () => {
-    return createStore(rootReducer)
+  return createStore(rootReducer,composeEnhancers())
 };
 
 export default configureStore;
