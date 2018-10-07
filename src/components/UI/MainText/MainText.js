6596d18a029c2302d7469ea396a1b55eab26dfd5
Luke De Feo <luke.defeo@just-eat.com>
2018-10-07 13:42:25 +0100

    added custom button and background image

--- src/components/UI/MainText/MainText.js
+++ src/components/UI/MainText/MainText.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import { StyleSheet, Text } from 'react-native';
 
+//you can wrap other text elements in this and the styles cascade, but probably better to jsut have the styles as constants
+//shared by other elements
 export default (props) => {
   return (
     <Text style={styles.mainText}>{props.children}</Text>
--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react'
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native'
 import { startTabs } from '../MainTabs/startMainTabs'
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput'
 import MainText from '../../components/UI/MainText/MainText'
+import BackGroundImage from '../../assets/background.jpg'
 import HeadingText from '../../components/UI/HeadingText/HeadingText'
+import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
 export default class AuthScreen extends Component {
 
   loginHandler = () => {
     startTabs()
   }
 
+  switchToLoginHandler = () => {
+
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <HeadingText>Please log in</HeadingText>
-        <Button title="Swith to login" />
-        <View style={styles.inputContainer}>
-          <DefaultInput placeholder="email" style={styles.style} />
-          <DefaultInput placeholder="password" style={styles.style}/>
-          <DefaultInput placeholder="confirm password" style={styles.style}/>
-        </View>
+      <ImageBackground style={styles.backGroundImage} source={BackGroundImage}>
+        <View style={styles.container}>
 
-        <Button onPress={this.loginHandler} title="Login22" />
-      </View>
+          <HeadingText>Please log in</HeadingText>
+          <ButtonWithBackground onPress={this.switchToLoginHandler} color="#29aaf4">"Swith to login"</ButtonWithBackground>
+          <View style={styles.inputContainer}>
+            <DefaultInput placeholder="email" style={styles.style} />
+            <DefaultInput placeholder="password" style={styles.style} />
+            <DefaultInput placeholder="confirm password" style={styles.style} />
+          </View>
+
+          <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler} >Login</ButtonWithBackground>
+
+        </View>
+      </ImageBackground >
+
     );
   }
 }
 
 const styles = StyleSheet.create({
-
+  backGroundImage: {
+    width: "100%",
+    flex: 1
+  },
   input: {
     borderWidth: 1,
     borderColor: "#eee"
